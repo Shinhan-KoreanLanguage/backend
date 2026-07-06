d21c87f171c6b8caebd28b5e85c7c862d4ba5512
kdh <soccerkdh12@naver.com>
2026-07-07 00:12:00 +0900

    [Setup/#1] feat: game_field_table 엔티티 설계

--- src/main/java/com/daehanforeigner/capstone/domain/game_field_table/entity/GameFieldTable.java
+++ src/main/java/com/daehanforeigner/capstone/domain/game_field_table/entity/GameFieldTable.java
@@ -1,4 +1,30 @@
 package com.daehanforeigner.capstone.domain.game_field_table.entity;
 
+import com.daehanforeigner.capstone.domain.game_result.entity.GameResult;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "game_field_table")
 public class GameFieldTable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "game_sentence_id") // 게임 문장 ID
+    private Long gameSentenceId;
+
+    @ManyToOne
+    @JoinColumn(name = "game_result_id") // 게임 결과 ID
+    private GameResult gameResult;
+
+    @Column(name = "is_sentence") // 문장 여부
+    private boolean isSentence;
+
+    @Column(name = "game_field") // 내용
+    private String gameField;
 }
