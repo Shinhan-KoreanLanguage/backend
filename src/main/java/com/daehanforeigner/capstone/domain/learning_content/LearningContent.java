6b268834935caa601955cb3a43a99f50496a4dd2
kdh <soccerkdh12@naver.com>
2026-07-06 13:54:35 +0900

    [Setup/#1] feat: learning_content 엔티티 설계

---
+++ src/main/java/com/daehanforeigner/capstone/domain/learning_content/LearningContent.java
@@ -0,0 +1,44 @@
+package com.daehanforeigner.capstone.domain.learning_content;
+
+import com.daehanforeigner.capstone.domain.content_category.ContentCategory;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "learning_content")
+public class LearningContent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "content_id")
+    private int contentId;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private ContentCategory contentCategory;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "content_type") // 음절, 단어, 문장
+    private ContentType contentType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "difficulty") // 쉬움, 보통, 난이도
+    private Difficulty difficulty;
+
+    @Column(name = "text") // 학습 텍스트
+    private String text;
+
+    @Column(name = "meaning") // 의미
+    private String meaning;
+
+    @Column(name = "example_sentence") // 예문
+    private String exampleSentence;
+
+    @Column(name = "pronunciation_guide") // 발음 가이드
+    private String pronunciationGuide;
+}
