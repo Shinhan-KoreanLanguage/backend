21d1788732cd68369b125a5d68914b245126a5c7
kdh <soccerkdh12@naver.com>
2026-07-07 00:11:56 +0900

    [Setup/#1] feat: game_result 엔티티 설계

--- src/main/java/com/daehanforeigner/capstone/domain/game_result/entity/GameResult.java
+++ src/main/java/com/daehanforeigner/capstone/domain/game_result/entity/GameResult.java
@@ -1,4 +1,40 @@
 package com.daehanforeigner.capstone.domain.game_result.entity;
 
+import com.daehanforeigner.capstone.domain.content_category.ContentCategory;
+import com.daehanforeigner.capstone.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "game_result")
 public class GameResult {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "game_result_id")
+    private Long gameResultId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private ContentCategory category;
+
+    @Column(name = "score")
+    private int score;
+
+    @Column(name = "is_passed")
+    private boolean isPassed;
+
+    @Column(name = "played_at")
+    private LocalDateTime playedAt;
 }
