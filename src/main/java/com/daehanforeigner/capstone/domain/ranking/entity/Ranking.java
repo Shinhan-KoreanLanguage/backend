4bba65b3f9bf5b03302790503b27c39c6d3eff1b
kdh <soccerkdh12@naver.com>
2026-07-07 00:11:34 +0900

    [Setup/#1] feat: ranking 엔티티 설계

--- src/main/java/com/daehanforeigner/capstone/domain/ranking/entity/Ranking.java
+++ src/main/java/com/daehanforeigner/capstone/domain/ranking/entity/Ranking.java
@@ -1,4 +1,30 @@
 package com.daehanforeigner.capstone.domain.ranking.entity;
 
+import com.daehanforeigner.capstone.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "ranking")
 public class Ranking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "rank_id")
+    private Long rankId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "best_score")
+    private int bestScore;
+
+    @Column(name = "spoken_word_count") // 말한 단어 수
+    private int spokenWordCount;
 }
