2f477551eb93335683be4e41f0cf918023383d38
kdh <soccerkdh12@naver.com>
2026-07-28 14:33:48 +0900

    [Feat/#8] Feat: @Transactional 어노테이션 readOnly = true 설정

--- src/main/java/com/daehanforeigner/capstone/domain/admin/service/AdminService.java
+++ src/main/java/com/daehanforeigner/capstone/domain/admin/service/AdminService.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class AdminService {
 
     private final UserRepository userRepository;
