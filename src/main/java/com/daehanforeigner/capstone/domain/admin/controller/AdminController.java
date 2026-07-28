f80b3ab6b4f509de5cd27a19aa4b910c1ec54485
kdh <soccerkdh12@naver.com>
2026-07-28 14:22:36 +0900

    [Feat/#8] Feat: AdminController 구현

--- src/main/java/com/daehanforeigner/capstone/domain/admin/controller/AdminController.java
+++ src/main/java/com/daehanforeigner/capstone/domain/admin/controller/AdminController.java
@@ -1,4 +1,24 @@
 package com.daehanforeigner.capstone.domain.admin.controller;
 
+import com.daehanforeigner.capstone.domain.admin.dto.AdminProfileResponseDTO;
+import com.daehanforeigner.capstone.domain.admin.service.AdminService;
+import com.daehanforeigner.capstone.global.rsdata.RsData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/admin")
 public class AdminController {
+
+    private final AdminService adminService;
+
+    @GetMapping
+    public ResponseEntity<RsData<AdminProfileResponseDTO>> getAdminProfile(@AuthenticationPrincipal Long userId) {
+        return ResponseEntity.ok(RsData.success(adminService.getAdminProfile(userId)));
+    }
 }
