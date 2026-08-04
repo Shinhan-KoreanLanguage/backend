46b984d789ac357d848847fe6b93ff2521857cf8
kdh <soccerkdh12@naver.com>
2026-08-04 14:29:45 +0900

    [Feat/#12] Feat: LearningContent 엔티티 update 메서드 category 추가

--- src/main/java/com/daehanforeigner/capstone/domain/learning_content/entity/LearningContent.java
+++ src/main/java/com/daehanforeigner/capstone/domain/learning_content/entity/LearningContent.java
@@ -46,6 +46,7 @@ public class LearningContent {
 
     // 학습 콘텐츠 정보 수정 처리 (controller단에서는 putMapping으로 처리)
     public void update(ContentType contentType, Difficulty difficulty, String text, String meaning, String exampleSentence, String pronunciationGuide) {
+        this.contentCategory = this.contentCategory;
         this.contentType = contentType;
         this.difficulty = difficulty;
         this.text = text;
