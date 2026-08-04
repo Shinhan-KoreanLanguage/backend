b1cb5e2382f068edc85102593d8f767abd083bd4
kdh <soccerkdh12@naver.com>
2026-08-04 16:07:11 +0900

    [Feat/#12] Feat: 관리자 카테고리 목록 조회 service, controller 구현

--- src/main/java/com/daehanforeigner/capstone/domain/admin/controller/AdminController.java
+++ src/main/java/com/daehanforeigner/capstone/domain/admin/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.daehanforeigner.capstone.domain.admin.controller;
 
 import com.daehanforeigner.capstone.domain.admin.dto.AdminProfileResponseDTO;
 import com.daehanforeigner.capstone.domain.admin.service.AdminService;
+import com.daehanforeigner.capstone.domain.content_category.dto.CategoryResponseDTO;
 import com.daehanforeigner.capstone.global.rsdata.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +23,10 @@ public class AdminController {
     @GetMapping
     public ResponseEntity<RsData<AdminProfileResponseDTO>> getAdminProfile(@AuthenticationPrincipal Long userId) {
         return ResponseEntity.ok(RsData.success(adminService.getAdminProfile(userId)));
+    }
+
+    @GetMapping("/categories")
+    public ResponseEntity<RsData<List<CategoryResponseDTO>>> getCategories() {
+        return ResponseEntity.ok(RsData.success(adminService.getCategories()));
     }
 }
