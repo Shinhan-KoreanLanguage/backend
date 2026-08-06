99d13f94156f39f3b73505c5d90e615657600a85
kdh <soccerkdh12@naver.com>
2026-08-06 13:51:27 +0900

    [Feat/#14] Feat: LearningContentRequestDTO 표준 발음, 모국어 발음 추가

--- src/main/java/com/daehanforeigner/capstone/domain/learning_content/dto/LearningContentRequestDTO.java
+++ src/main/java/com/daehanforeigner/capstone/domain/learning_content/dto/LearningContentRequestDTO.java
@@ -25,7 +25,11 @@ public record LearningContentRequestDTO(
 
         String exampleSentence,
 
-        String pronunciationGuide
+        String pronunciationGuide,
+
+        String standardPronunciationText, // 표준 발음 표기
+
+        String nativePronunciation // 모국어 발음 표기
 ) {
         // DTO를 엔티티로 변환하는 메서드
         public LearningContent toEntity(ContentCategory contentCategory) {
@@ -37,6 +41,8 @@ public record LearningContentRequestDTO(
                     .meaning(meaning)
                     .exampleSentence(exampleSentence)
                     .pronunciationGuide(pronunciationGuide)
+                    .standardPronunciationText(standardPronunciationText)
+                    .nativePronunciation(nativePronunciation)
                     .build();
         }
 }
