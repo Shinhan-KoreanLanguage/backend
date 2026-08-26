e46e774bea24211387fca367dfdc6019247a5251
kdh <soccerkdh12@naver.com>
2026-08-26 17:20:01 +0900

    [Feat/#35] Fix: Object Storage 업로드 시 체크섬 형식으로 403이 나는 문제 수정

--- src/main/java/com/daehanforeigner/capstone/global/config/ObjectStorageConfig.java
+++ src/main/java/com/daehanforeigner/capstone/global/config/ObjectStorageConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.checksums.RequestChecksumCalculation;
+import software.amazon.awssdk.core.checksums.ResponseChecksumValidation;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -39,6 +41,10 @@ public class ObjectStorageConfig {
                 // 기본값(virtual-host 방식)은 버킷명을 도메인 앞에 붙이는데,
                 // NCP는 "엔드포인트/버킷명/파일경로" 형태를 쓰므로 path 방식으로 고정한다
                 .forcePathStyle(true)
+                // AWS SDK 2.30+는 업로드 시 본문을 aws-chunked로 쪼개고 끝에 체크섬을 붙이는데,
+                // NCP가 이 형식을 해석하지 못해 403이 난다. 꼭 필요할 때만 붙이도록 낮춘다
+                .requestChecksumCalculation(RequestChecksumCalculation.WHEN_REQUIRED)
+                .responseChecksumValidation(ResponseChecksumValidation.WHEN_REQUIRED)
                 .build();
     }
 }
